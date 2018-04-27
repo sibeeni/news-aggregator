40889621cca624ca42fc5d0f84d2dc7e592d8a85
sibeeni <sibeeni@gmail.com>
2018-04-27 22:53:09 +0700

    22

--- scripts/app.js
+++ scripts/app.js
@@ -191,7 +191,8 @@ APP.Main = (function() {
 
       // Set up the next bit of the animation if there is more to do.
       if (Math.abs(left) > 0.5)
-        setTimeout(animate, 4);
+        //setTimeout(animate, 4);
+		requestAnimationFrame(animate);
       else
         left = 0;
 
@@ -204,7 +205,8 @@ APP.Main = (function() {
     // every few milliseconds. That's going to keep
     // it all tight. Or maybe we're doing visual changes
     // and they should be in a requestAnimationFrame
-    setTimeout(animate, 4);
+    //setTimeout(animate, 4);
+	requestAnimationFrame(animate);
   }
 
   function hideStory(id) {
@@ -230,7 +232,8 @@ APP.Main = (function() {
 
       // Set up the next bit of the animation if there is more to do.
       if (Math.abs(left - target) > 0.5) {
-        setTimeout(animate, 4);
+        //setTimeout(animate, 4);
+		requestAnimationFrame(animate);
       } else {
         left = target;
         inDetails = false;
@@ -245,7 +248,8 @@ APP.Main = (function() {
     // every few milliseconds. That's going to keep
     // it all tight. Or maybe we're doing visual changes
     // and they should be in a requestAnimationFrame
-    setTimeout(animate, 4);
+    //setTimeout(animate, 4);
+	requestAnimationFrame(animate);
   }
 
   /**
